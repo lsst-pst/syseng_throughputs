e86629d35ac474708b3d75d7d9da791968adaa57
Lynne Jones <lynnej@uw.edu>
2015-11-04 16:43:23 -0800

    Added fit for effective wavelength with X

--- python/effectiveWavelen.py
+++ python/effectiveWavelen.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 from lsst.sims.photUtils import Bandpass, Sed, PhotometricParameters, LSSTdefaults
 import bandpassUtils as bu
 
@@ -10,7 +11,8 @@ def calcEffWavelen(hardware, title):
     stdAtmoFile = os.path.join(os.getenv('SYSENG_THROUGHPUTS_DIR'), 'siteProperties/pachonModtranAtm_12.dat')
     atmos['std'] = Bandpass()
     atmos['std'].readThroughput(stdAtmoFile)
-    for X in np.arange(1.0, 2.55, 0.1):
+    Xarr = np.arange(1.0, 2.55, 0.1)
+    for X in Xarr:
         atmos['%.1f' %X] = Bandpass()
         atmos['%.1f' %X].readThroughput(os.path.join(os.getenv('THROUGHPUTS_DIR'), 'atmos/atmos_%d.dat' %(int(X*10))))
     atmoskeys = sorted(atmos.keys())
@@ -26,6 +28,17 @@ def calcEffWavelen(hardware, title):
             writestring += '%.2f ' %(effsb[k])
         print writestring
 
+        effsb_vals = np.zeros(len(Xarr), float)
+        for i, X in enumerate(Xarr):
+            effsb_vals[i] = effsb['%.1f' %X]
+        p = np.polyfit(Xarr, effsb_vals, 1)
+        print p
+        effsb_eval = np.polyval(p, Xarr) 
+        print (effsb_vals - effsb_eval).max(), (effsb_vals-effsb_eval).std()
+        plt.plot(Xarr, effsb_vals-effsb_eval, label=f)
+    plt.legend()
+    plt.show()
+
 if __name__ == '__main__':
 
     defaultDirs = bu.setDefaultDirs(rootDir = '..')
@@ -36,7 +49,7 @@ if __name__ == '__main__':
     defaultDirs['detector'] = os.path.join(os.getenv('SYSENG_THROUGHPUTS_DIR'), 'components/camera/detector')
     hardware, system = bu.buildHardwareAndSystem(defaultDirs, addLosses)
     calcEffWavelen(hardware, 'Min: Std Atmo')
-
+    exit()
     # vendor1 detector throughputs
     defaultDirs['detector'] = os.path.join(os.getenv('SYSENG_THROUGHPUTS_DIR'), 'components/camera/detector/vendor1')
     hardware, system = bu.buildHardwareAndSystem(defaultDirs, addLosses)
