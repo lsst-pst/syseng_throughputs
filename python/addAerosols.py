5d772aa9f9bd3546f0e9d24347c3702d64aad29b
Lynne Jones <lynnej@uw.edu>
2015-12-07 17:28:52 -0800

    Added X dependence to aerosols

--- python/addAerosols.py
+++ python/addAerosols.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 from lsst.sims.photUtils import Bandpass
 import bandpassUtils as bu
 
-def addAerosol(atmosphere, tau0=0.05, alpha=1.0, wavelen0=440.0, plotAtmosphere=True):
+def addAerosol(atmosphere, X, tau0=0.05, alpha=1.0, wavelen0=440.0, plotAtmosphere=True):
     # Calculate the aerosol contribution -- sb with aerosols = sb*exp(-tau)
     tau = tau0 * np.power((wavelen0/atmosphere.wavelen), alpha)
     # Generate new atmosphere bandpass with aerosols.
     atmosphere_aerosol = Bandpass()
     atmosphere_aerosol.setBandpass(wavelen = atmosphere.wavelen,
-                                   sb = atmosphere.sb * np.exp(-tau))
+                                   sb = atmosphere.sb * np.exp(-tau*X))
 
     if plotAtmosphere:
         # Plot for a check:
@@ -24,12 +24,13 @@ if __name__=='__main__':
 
     parser = argparse.ArgumentParser(description='Add a standard aerosol component to an atmosphere without aerosols')
     parser.add_argument('atmosphereFile', type=str, default=None, help='The atmosphere file to add aerosols to.')
+    parser.add_argument('airmass', type=float, default=None, help='The airmass of the atmosphere file.')
     args = parser.parse_args()
     atmosDir, atmosFile = os.path.split(args.atmosphereFile)
 
     defaultDirs = bu.setDefaultDirs()
     atmosphere = bu.readAtmosphere(atmosDir, atmosFile)
-    atmosphere_aerosol = addAerosol(atmosphere)
+    atmosphere_aerosol = addAerosol(atmosphere, args.airmass)
 
     outfile = atmosphere.bandpassname.replace('.dat', '') + '_aerosol.dat'
     atmosphere_aerosol.writeThroughput(outfile, print_header='Added aerosol component')
